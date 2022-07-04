905ca4dc8620b64a13fbd400d36861522b9492c5
Gaspare Iengo <gaspareiengo@gmail.com>
2022-07-04 13:05:44 +0000

    Added shutdownWorkers
    Using it instead of killWorkers in the destructor

--- src/Worker/DefaultWorkerPool.php
+++ src/Worker/DefaultWorkerPool.php
@@ -74,7 +74,8 @@ final class DefaultWorkerPool implements WorkerPool
     public function __destruct()
     {
         if ($this->isRunning()) {
-            self::killWorkers($this->workers, $this->waiting);
+            // self::killWorkers($this->workers, $this->waiting);
+            self::shutdownWorkers($this->workers, $this->waiting);
         }
     }
 
@@ -177,6 +178,18 @@ final class DefaultWorkerPool implements WorkerPool
             \assert($worker instanceof Worker);
             if ($worker->isRunning()) {
                 $worker->kill();
+            }
+        }
+
+        $waiting?->error(new WorkerException('The pool was killed before the task could be executed'));
+    }
+    
+    private static function shutdownWorkers(\SplObjectStorage $workers, ?DeferredFuture $waiting): void
+    {
+        foreach ($workers as $worker) {
+            \assert($worker instanceof Worker);
+            if ($worker->isRunning()) {
+                $worker->shutdown();
             }
         }
 
